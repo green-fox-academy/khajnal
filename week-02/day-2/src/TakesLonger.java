678f44aab915b02033cfbba8a94e37bcc051997b
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-07 13:09:26 +0200

    TodoPrint.java added

--- week-02/day-2/src/TakesLonger.java
+++ week-02/day-2/src/TakesLonger.java
@@ -1,2 +1,7 @@
 public class TakesLonger {
+  public static void main(String[] args) {
+    String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+    quote = quote.substring(0,20) + " always takes longer than" + quote.substring(20);
+    System.out.println(quote);
+  }
 }
