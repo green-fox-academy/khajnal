f68e0b6a332a4e029a1789ae9655e4675fa3b6ca
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-14 12:48:10 +0200

    finished

--- week-03/day-2/src/PrintEachLine.java
+++ week-03/day-2/src/PrintEachLine.java
@@ -1,14 +1,14 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class PrintEachLine {
   public static void main(String[] args) {
     try {
-      Path filepath = Paths.get(~/Documents/Greenfox/my-file.txt);
+      Path filepath = Paths.get("~/Documents/Greenfox/my-file.txt");
       System.out.println(Files.readAllLines(filepath));
-    } catch () {
+    } catch (IOException a) {
       System.out.println("Unable to read file: my-file.txt");
     }
   }
