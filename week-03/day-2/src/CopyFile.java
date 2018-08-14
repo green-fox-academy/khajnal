567666a85f24636ee3bbed2536693b3f2ae50c41
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-14 14:43:48 +0200

    Paths created

--- week-03/day-2/src/CopyFile.java
+++ week-03/day-2/src/CopyFile.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CopyFile {
+  public static void main(String[] args) {
+    Path filePathA = Paths.get("/home/kriszti/Documents/Greenfox/originalFile.txt");
+    Path filePathB = Paths.get("/home/kriszti/Documents/Greenfox/copyFile.txt");
+    copyFile(filePathA, filePathB);
+  }
 }
