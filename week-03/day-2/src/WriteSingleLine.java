0824bc336d93d18098191f59646eb02302ca7efa
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-14 13:51:27 +0200

    try-catch blocks finished

--- week-03/day-2/src/WriteSingleLine.java
+++ week-03/day-2/src/WriteSingleLine.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class WriteSingleLine {
+  public static void main(String[] args) {
+    Path filepath = Paths.get("/home/kriszti/Documents/Greenfox/my-file.txt");
+    ArrayList<String> content = new ArrayList<>();
+    content.add("Kriszti");
+    try {
+      Files.write(filepath, content);
+      System.out.println(Files.readAllLines(filepath));
+    } catch (IOException a) {
+      System.out.println("Unable to write file: my-file.txt");
+    }
+  }
 }
