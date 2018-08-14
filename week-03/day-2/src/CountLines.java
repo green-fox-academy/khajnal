597fca91ba46f7ffa27ba8303846517ec09dc7d5
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-14 13:33:44 +0200

    Finished method

--- week-03/day-2/src/CountLines.java
+++ week-03/day-2/src/CountLines.java
@@ -1,18 +1,18 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class CountLines {
   public static void main(String[] args) {
-    Path filePath = Paths.get("~/Documents/Greenfox/my-file.txt");
+    Path filePath = Paths.get("/home/kriszti/Documents/Greenfox/my-file.txt");
     System.out.println(countLines(filePath));
   }
-
   private static int countLines(Path filePath) {
     try {
-
-    } catch () {
-      
+      return Files.readAllLines(filePath).size();
+    } catch (IOException a) {
+      return 0;
     }
   }
 }
