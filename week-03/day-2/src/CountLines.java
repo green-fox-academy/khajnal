9daa4eda735b002f048a6c6182ed6117c3b85e57
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-14 13:05:18 +0200

    Fixed URL

---
+++ week-03/day-2/src/CountLines.java
@@ -0,0 +1,9 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CountLines {
+  public static void main(String[] args) {
+    Path filePath = Paths.get("~/Documents/Greenfox/my-file.txt");
+  }
+}
