ae4ee77530fd2d15f74e5f549c81142222b82f64
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-16 10:59:18 +0200

    Loop created

--- week-03/day-3/Drawing/src/Counter.java
+++ week-03/day-3/Drawing/src/Counter.java
@@ -1,12 +1,13 @@
 public class Counter {
   public static void main(String[] args) {
-
+    int number = 10;
+    System.out.println(finalCountDown(number));
   }
   public static int finalCountDown(int n) {
     if (n == 0) {
       return 0;
     } else {
-
+      return finalCountDown(n - 1);
     }
   }
 }
---
+++ week-03/day-3/Drawing/src/NumberAdder.java
@@ -0,0 +1,5 @@
+public class NumberAdder {
+  public static void main(String[] args) {
+
+  }
+}
