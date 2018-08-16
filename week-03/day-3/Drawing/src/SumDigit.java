5f489e4354f737deab6a1adbded6427d225cf3e5
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-16 11:41:56 +0200

    method finished

--- week-03/day-3/Drawing/src/SumDigit.java
+++ week-03/day-3/Drawing/src/SumDigit.java
@@ -1,5 +1,13 @@
 public class SumDigit {
   public static void main(String[] args) {
-
+    int number = 1336;
+    System.out.println(sumDigits(number));
+  }
+  private static int sumDigits(int number) {
+    if (number == 0) {
+      return 0;
+    } else {
+      return number % 10 + sumDigits(number / 10);
+    }
   }
 }
