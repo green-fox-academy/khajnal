b47e0868d36701f15a03656ec75b43e76c47fd3b
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-16 11:20:58 +0200

    Function created

--- week-03/day-3/Drawing/src/NumberAdder.java
+++ week-03/day-3/Drawing/src/NumberAdder.java
@@ -1,5 +1,13 @@
 public class NumberAdder {
   public static void main(String[] args) {
-
+    int number = 5;
+    System.out.println(addFromOne(number));
+  }
+  private static int addFromOne(int number) {
+    if (number == 1) {
+      return number;
+    } else {
+      return number + addFromOne(number -  1);
+    }
   }
 }
---
+++ week-03/day-3/Drawing/src/SumDigit.java
@@ -0,0 +1,2 @@
+public class SumDigit {
+}
