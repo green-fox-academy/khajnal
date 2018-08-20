313d441a17511e25a7eaa8cdf4a778436301e93d
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-20 19:53:25 +0200

    Created introduction and getgoal method

--- week-04/day-2/src/Person.java
+++ week-04/day-2/src/Person.java
@@ -1,12 +1,19 @@
 public class Person {
   String name;
   int age;
-  boolean female;
+  String gender;
 
-  public Person(String name, int age, boolean female) {
+  public Person(String name, int age, String maleOrFemale) {
     this.name = name;
     this.age = age;
-    this.female = female;
+    gender = maleOrFemale;
   }
-  
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + " , a " + age + " year old " + gender + ".");
+  }
+
+  public void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
 }
