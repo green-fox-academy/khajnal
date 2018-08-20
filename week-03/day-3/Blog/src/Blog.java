60b3f1504109f0bb3fab0da507fb771a31095309
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-20 17:48:50 +0200

    Update function finished, task finished

--- week-03/day-3/Blog/src/Blog.java
+++ week-03/day-3/Blog/src/Blog.java
@@ -3,16 +3,27 @@ import java.util.List;
 
 public class Blog {
   List<BlogPost> listofPosts = new ArrayList<>();
+  String name;
 
+  public Blog(String name) {
+    this.name = name;
+  }
   public void addPost(BlogPost name) {
     listofPosts.add(name);
   }
   public void delete(int index) {
     listofPosts.remove(index);
   }
-  public void checkListTitles(){
+  public void checkListTitles() {
     for (int i = 0; i < listofPosts.size(); i++) {
       System.out.println(listofPosts.get(i).title);
     }
+  }
+  public List<BlogPost> update(int index, BlogPost blogpost) {
+    listofPosts.set(index, blogpost);
+    return listofPosts;
+  }
+  public void checkText(int index) {
+    System.out.println(listofPosts.get(index).text);
   }
 }
--- week-03/day-3/Blog/src/Main.java
+++ week-03/day-3/Blog/src/Main.java
@@ -3,12 +3,17 @@ public class Main {
     BlogPost lorem = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
     BlogPost wait = new BlogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
     BlogPost one = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
-    Blog list = new Blog();
+    Blog list = new Blog("Blabla Blog");
     list.addPost(lorem);
     list.addPost(wait);
     list.addPost(one);
 
     list.delete(2);
     list.checkListTitles();
+
+    BlogPost newOne = new BlogPost("Krisztinka", "Krisztinka's world", "I keep a close watch on this heart of mine \nI keep my eyes wide open all the time", "2018.08.20.");
+    list.update(0, newOne);
+    list.checkListTitles();
+    list.checkText(0);
   }
 }
