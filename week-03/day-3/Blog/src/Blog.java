7b8a878bc36ac21a4d5d54c08f987b83170215d4
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-20 17:26:01 +0200

    delete function working

--- week-03/day-3/Blog/src/Blog.java
+++ week-03/day-3/Blog/src/Blog.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Blog {
+  List<BlogPost> listofPosts = new ArrayList<>();
+
+  public void addPost(BlogPost name) {
+    listofPosts.add(name);
+  }
+  public void delete(int index) {
+    listofPosts.remove(index);
+  }
+  public void checkListTitles(){
+    for (int i = 0; i < listofPosts.size(); i++) {
+      System.out.println(listofPosts.get(i).title);
+    }
+  }
 }
