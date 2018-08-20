dadf656fbe0c831041b1a8b4db7abe0b323546f8
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-20 19:46:43 +0200

    Person created

---
+++ week-04/day-2/src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+  String name;
+  int age;
+  boolean female;
+
+  public Person(String name, int age, boolean female) {
+    this.name = name;
+    this.age = age;
+    this.female = female;
+  }
+  
+}
