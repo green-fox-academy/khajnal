70481ba4680b7c5667d0134d5276250101732813
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-21 12:46:04 +0200

    Task finished, tested

--- week-04/day-2/GreenFox Inheritance/src/Cohort.java
+++ week-04/day-2/GreenFox Inheritance/src/Cohort.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cohort {
   String name;
-  List students;
-  List mentors;
+  List<Student> students = new ArrayList();
+  List<Mentor> mentors = new ArrayList<>();
 
+  public Cohort(String name) {
+    this.name = name;
+  }
+
+  public void addStudent(Student student) {
+    students.add(student);
+  }
+
+  public void addMentor(Mentor mentor) {
+    mentors.add(mentor);
+  }
+
+  public void info() {
+    System.out.println("The " + name + " cohort has " + students.size() + " students and " + mentors.size() + " mentors.");
+  }
 }
-  Create a Cohort class that has the following
-    methods:
-    addStudent(Student): adds the given Student to students list
-    addMentor(Mentor): adds the given Mentor to mentors list
-    info(): prints out "The name cohort has students.size() students and mentors.size() mentors."
-    The Cohort class has the following constructors:
-
-    Cohort(name): beside the given parameter, it sets students and mentors as empty lists
