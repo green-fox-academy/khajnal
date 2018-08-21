a09c048b01e3ef043037b89f2fc5b353213cd164
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-21 17:40:07 +0200

    Still not working

--- week-04/day-2/Aircraft/src/Main.java
+++ week-04/day-2/Aircraft/src/Main.java
@@ -2,14 +2,16 @@ public class Main {
   public static void main(String[] args) {
     Aircraft f16 = new F16();
     Aircraft f35 = new F35();
+    Aircraft f35sec = new F35();
 
     Carrier myarmy = new Carrier(19, 500);
     myarmy.add(f16);
     myarmy.add(f35);
+    myarmy.add(f35sec);
     myarmy.fill();
 
     System.out.println(f16.getStatus());
     System.out.println(f35.getStatus());
-
+    System.out.println(f35sec.getStatus());
   }
 }
