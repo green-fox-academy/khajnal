10a32f99cb99610decdced0a070eacbce17f2eda
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-21 11:49:33 +0200

    Field variables for cohort class created

--- week-04/day-2/GreenFox Inheritance/src/Cohort.java
+++ week-04/day-2/GreenFox Inheritance/src/Cohort.java
@@ -1,3 +1,16 @@
+import java.util.List;
+
 public class Cohort {
-  
+  String name;
+  List students;
+  List mentors;
+
 }
+  Create a Cohort class that has the following
+    methods:
+    addStudent(Student): adds the given Student to students list
+    addMentor(Mentor): adds the given Mentor to mentors list
+    info(): prints out "The name cohort has students.size() students and mentors.size() mentors."
+    The Cohort class has the following constructors:
+
+    Cohort(name): beside the given parameter, it sets students and mentors as empty lists
