562cdbdd82db1a11f7fe526987a874b57708003f
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-23 17:25:03 +0200

    task started

---
+++ week-04/day-4/src/Letters.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Letters {
+
+  public HashMap<Character, Integer> countLetters(String letters) {
+    for (int i = 0; i < letters.length(); i++) {
+
+    }
+  }
+}
