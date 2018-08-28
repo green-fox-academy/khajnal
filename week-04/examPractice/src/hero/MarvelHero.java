e1ce90b9a3fe0bd20c52c7791fa03a60c3e9666d
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-28 19:54:16 +0200

    Fixed motlvl

--- week-04/examPractice/src/hero/MarvelHero.java
+++ week-04/examPractice/src/hero/MarvelHero.java
@@ -23,4 +23,18 @@ public class MarvelHero extends Hero {
       }
     }
   }
+
+// Other solution for punch, using instanceof
+
+//  @Override
+//  public void punch(Punchable other) {
+//    if (other instanceof MarvelHero == false) {
+//      if (getMotivationLevel() >= 1) {
+//        double damage = motivation / 1.5;
+//        bePunched(damage);
+//      } else {
+//        System.out.println("I am not motivated enough");
+//      }
+//    }
+//  }
 }
