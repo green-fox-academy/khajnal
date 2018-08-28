15498ddfeb9c6c57e4a77a83d9214d49a22d0595
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-28 15:27:37 +0200

    Order reversed, but still in a List

--- week-04/examPractice/src/ReversedLines.java
+++ week-04/examPractice/src/ReversedLines.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ReversedLines {
   public static void main(String[] args) {
     String filename = "text/reversedLines.csv";
-    decryptReversedLines(filename);
+    System.out.println(decryptReversedLines(filename));
   }
 
   private static List<String> decryptReversedLines(String filename) {
@@ -19,9 +19,17 @@ public class ReversedLines {
     } catch (Exception e) {
       System.out.println("Cannot read the file");
     }
+    char oneLetter = '*';
+    String oneLine = "";
+    for (int i = 0; i < encryptedText.size(); i++) {
+      for (int j = encryptedText.get(i).length() - 1; j > 0; j--) {
+        oneLetter = encryptedText.get(i).charAt(j);
+        oneLine += oneLetter;
+      }
+      decryptedText.add(oneLine);
+    }
 
-    
-    return
+    return decryptedText;
   }
 
 }
