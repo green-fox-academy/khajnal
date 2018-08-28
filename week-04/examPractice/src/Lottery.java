de0e1abba5fe7f4df5275784c52fc204c3954176
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-28 14:42:28 +0200

    Hashmap filled with the lottery numbers

--- week-04/examPractice/src/Lottery.java
+++ week-04/examPractice/src/Lottery.java
@@ -1,23 +1,37 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lottery {
   public static void main(String[] args) {
-    commonLotteryNumbers();
+    System.out.println(commonLotteryNumbers());
   }
 
-  private static List<Integer> commonLotteryNumbers() {
+  private static Map<String,Integer> commonLotteryNumbers() {
+    List<String> lines = new ArrayList<>();
+    Map<String, Integer> numbers = new HashMap<>();
     try {
-      Path path = Paths.get("/text/lottery.csv");
-      List<String> lines = Files.readAllLines(path);
+      Path path = Paths.get("text/lottery.csv");
+      lines = Files.readAllLines(path);
     } catch (Exception e) {
       System.out.println("Cannot read the file");
-      for (int i = 0; i < ; i++) {
-        
-      }
     }
-    return
+      for (int i = 0; i < lines.size(); i++) {
+        String[] splittedLine = lines.get(i).split(";");
+        for (int j = 1; j < 6; j++) {
+          String oneNumber = splittedLine[splittedLine.length - j];
+          if (!numbers.containsKey(oneNumber)) {
+            numbers.put(oneNumber, 1);
+          } else {
+            int temp = numbers.get(oneNumber);
+            numbers.put(oneNumber, temp + 1);
+          }
+        }
+      }
+    return numbers;
   }
 }
