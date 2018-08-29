0f6f27dbf6a40a5b42c5a90e312191e6bb5c1bc1
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-29 13:52:49 +0200

    variable name modified

--- week-04/examPractice/src/SubInt.java
+++ week-04/examPractice/src/SubInt.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubInt {
   public static void main(String[] args) {
     //  Create a function that takes a number and a list of numbers as a parameter
@@ -5,13 +9,21 @@ public class SubInt {
     //  Returns an empty list if the number is not part any of the numbers in the list
 
     //  Example:
-    System.out.println(subInt(1, new int[] {1, 11, 34, 52, 61}));
+    System.out.println(subInt(1, new int[]{1, 11, 34, 52, 61}));
     //  should print: `[0, 1, 4]`
-    System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
+    System.out.println(subInt(9, new int[]{1, 11, 34, 52, 61}));
     //  should print: '[]'
   }
 
-  private static int[] subInt(int i, int[] ints) {
-    
+  private static List<Integer> subInt (int number, int[] ints) {
+    String indexString = String.valueOf(number);
+    List<Integer> indicesIfContains = new ArrayList<>();
+    for (int i = 0; i < ints.length; i++) {
+      String oneElement = String.valueOf(ints[i]);
+      if (oneElement.contains(indexString)) {
+       indicesIfContains.add(i);
+      }
+    }
+    return indicesIfContains;
   }
 }
