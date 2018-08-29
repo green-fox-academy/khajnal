9ef85e81e31be81fc090cf2096bbbfdb385740a1
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-29 16:14:36 +0200

    Three testcases done

--- week-05/day-3/examPractice/tests/UniqueCharsTest.java
+++ week-05/day-3/examPractice/tests/UniqueCharsTest.java
@@ -18,10 +18,7 @@ public class UniqueCharsTest {
   @Test
   public  void uniqueCharacterTestIfThereIsUniqueChar() {
     String test = "apple";
-    List<String> testUnique = new ArrayList<>();
-    testUnique.add("a");
-    testUnique.add("l");
-    testUnique.add("e");
+    List<String> testUnique = new ArrayList<>(Arrays.asList("a", "l", "e"));
     boolean isUnique = false;
     for (int i = 0; i < testUnique.size(); i++) {
       if (UniqueChars.uniqueCharacters(test).contains(testUnique.get(i))) {
@@ -34,20 +31,13 @@ public class UniqueCharsTest {
   @Test
   public  void uniqueCharacterTestIfOnlyUniqueCharacter() {
     String test = "zsiraf";
-    List<String> testUnique = new ArrayList<>();
-    appleUnique.add("a");
-    appleUnique.add("l");
-    appleUnique.add("e");
+    List<String> testUnique = new ArrayList<>(Arrays.asList("z", "s", "i", "r", "a", "f"));
     boolean isUnique = false;
-    for (int i = 0; i < appleUnique.size(); i++) {
-      if (UniqueChars.uniqueCharacters(test).contains(appleUnique.get(i))) {
+    for (int i = 0; i < testUnique.size(); i++) {
+      if (UniqueChars.uniqueCharacters(test).contains(testUnique.get(i))) {
         isUnique = true;
       }
     }
     assertTrue(isUnique);
   }
-//
-//  @Test
-//
-//  @Test
 }
