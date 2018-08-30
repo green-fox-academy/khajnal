abe7dc185d22a00e4280f8207fe3dcc341e0bbca
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-30 13:26:19 +0200

    Methods are working, finished task

--- week-05/day-3/examPractice/src/EncodedLines.java
+++ week-05/day-3/examPractice/src/EncodedLines.java
@@ -7,11 +7,10 @@ import java.util.List;
 
 public class EncodedLines {
   public static void main(String[] args) {
-    Path destPath = Paths.get("texts/decrypted-encode.txt");
-    List<String> decryptedLines = new ArrayList<>();
+    printContent("texts/encoded-lines.txt");
   }
 
-  public List<String> readAllLines(String filename) {
+  public static List<String> readAllLines(String filename) {
     Path path = Paths.get(filename);
     List<String> decryptedLines = new ArrayList<>();
     try {
@@ -22,11 +21,27 @@ public class EncodedLines {
     return decryptedLines;
   }
 
-  public List<String> encodeContent(String filename)  {
+  public static List<String> decryptContent(String filename)  {
     List<String> decryptedLines = readAllLines(filename);
     List<String> encryptedLines = new ArrayList<>();
     for (String oneLine : decryptedLines) {
+      String decryptedLine = "";
+      for (int i = 0; i < oneLine.length(); i++) {
+        if (oneLine.charAt(i) != ' ') {
+          decryptedLine += String.valueOf(Character.toChars(oneLine.charAt(i) - 1));
+        } else {
+          decryptedLine += ' ';
+        }
+      }
+      encryptedLines.add(decryptedLine);
+    }
+    return encryptedLines;
+  }
 
+  public static void printContent(String filename) {
+    List<String> encryptedLines = decryptContent(filename);
+    for (String oneLine : encryptedLines) {
+      System.out.println(oneLine);
     }
   }
 }
