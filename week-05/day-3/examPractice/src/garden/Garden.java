99e8075415721d8251ef607f40aa66910ba83748
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-30 09:55:37 +0200

    Init classes

--- week-05/day-3/examPractice/src/garden/Garden.java
+++ week-05/day-3/examPractice/src/garden/Garden.java
@@ -1,4 +1,16 @@
 package garden;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Garden {
+  List<Plants> garden = new ArrayList<>();
+
+  public String getStatus() {
+    String status = "";
+    for (Plants onePlant : garden) {
+      if (onePlant instanceof Flower)
+    }
+  }
+  }
 }
---
+++ week-05/day-3/examPractice/src/garden/Plants.java
@@ -0,0 +1,6 @@
+package garden;
+
+public abstract class Plants {
+  private int waterLvl;
+  private String color;
+}
