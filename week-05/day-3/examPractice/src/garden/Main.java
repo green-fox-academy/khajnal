968739b0f3dca099c5d1ffe5ce0843995fc59f97
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-08-30 10:07:38 +0200

    Garden created in Main, and getStatus working without waterlvl

--- week-05/day-3/examPractice/src/garden/Main.java
+++ week-05/day-3/examPractice/src/garden/Main.java
@@ -1,9 +1,20 @@
 package garden;
 
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
     Garden myGarden = new Garden();
-    Flower flowerOne = new Flower();
-    myGarden.addPlant();
+    Flower flowerOne = new Flower("yellow");
+    Flower flowerTwo = new Flower("blue");
+    Tree treeOne = new Tree("purple");
+    Tree treeTwo = new Tree("orange");
+
+    myGarden.addPlant(flowerOne);
+    myGarden.addPlant(flowerTwo);
+    myGarden.addPlant(treeOne);
+    myGarden.addPlant(treeTwo);
+
+    System.out.println(myGarden.getGardenStatus());
   }
 }
