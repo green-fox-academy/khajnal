f87eeb9ed1c51dfd08e849aa50c16cda9f5d853a
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-09-09 19:35:50 +0200

    Exercise6 init

---
+++ week-06/day-1/src/Exercise6.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise6 {
+  public static void main(String[] args) {
+    ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+    
+  }
+}
