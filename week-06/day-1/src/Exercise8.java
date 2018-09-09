1e20dc69777ed01ac8a78cf24e12257fb870be9a
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-09-09 22:07:51 +0200

    Exercise8 ready with method

---
+++ week-06/day-1/src/Exercise8.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exercise8 {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+
+    System.out.println(nrFrequencyMethod(numbers));
+  }
+
+  private static Map<Integer, Integer> nrFrequencyMethod(ArrayList<Integer> example) {
+    Map<Integer, Integer> nrFrequency = new HashMap<>();
+    for (Integer oneElement : example) {
+      if (!nrFrequency.containsKey(oneElement)) {
+        nrFrequency.put(oneElement, 1);
+      } else {
+        nrFrequency.put(oneElement, nrFrequency.get(oneElement) + 1);
+      }
+    }
+    return nrFrequency;
+  }
+}
