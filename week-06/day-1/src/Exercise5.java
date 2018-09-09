4c5f53c85bc86413d41a86accbaaeda6d852ffc8
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-09-09 19:34:53 +0200

    merged String working

--- week-06/day-1/src/Exercise5.java
+++ week-06/day-1/src/Exercise5.java
@@ -1,5 +1,22 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Exercise5 {
   public static void main(String[] args) {
-    
+    String poem = "Tyger Tyger, burning bright, In the forests of the night";
+
+    List<Character> upperCaseLetters = poem.chars()
+      .filter(Character :: isUpperCase)
+      .mapToObj(Character -> (char) Character)
+      .collect(Collectors.toList());
+
+    System.out.println(upperCaseLetters);
+
+    String mergedString = poem.chars()
+      .filter(Character :: isUpperCase)
+      .mapToObj(Character -> String.valueOf((char)Character))
+      .collect(Collectors.joining(", "));
+
+    System.out.println(mergedString);
   }
 }
