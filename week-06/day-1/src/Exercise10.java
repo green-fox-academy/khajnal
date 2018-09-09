4731b5e13b988bbdb11f3b2fc7fb01883e0de8ca
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-09-09 20:12:48 +0200

    Green foxes found

--- week-06/day-1/src/Exercise10.java
+++ week-06/day-1/src/Exercise10.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise10 {
   public static void main(String[] args) {
@@ -11,7 +12,16 @@ public class Exercise10 {
                                                     new Fox("becky", "pallinda", "orange"),
                                                     new Fox("bobby", "pallinda", "green")));
 
-    List<Fox> = foxes.stream()
-      .filter();
+//    List<Fox> greenFoxes = foxes.stream()
+//      .filter(fox -> fox.getcolor().equals("green"))
+//      .collect(Collectors.toList());
+//    System.out.println(greenFoxes);
+
+    List<String> greenFoxes = foxes.stream()
+      .filter(fox -> fox.getcolor().equals("green"))
+      .map(Fox::getName)
+      .collect(Collectors.toList());
+
+    System.out.println(greenFoxes);
   }
 }
