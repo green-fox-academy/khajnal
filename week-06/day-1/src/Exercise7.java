8f3e07cf35a62ecb59bbd00fb62038d38684cea0
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-09-09 22:04:31 +0200

    Exercise7 ready with method

--- week-06/day-1/src/Exercise7.java
+++ week-06/day-1/src/Exercise7.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercise7 {
   public static void main(String[] args) {
-    
+    String poem = "Tyger Tyger, burning bright, In the forests of the night";
+
+    System.out.println(charFrequencyMethod(poem));
+  }
+
+  private static Map<Character, Integer> charFrequencyMethod(String example) {
+    Map<Character, Integer> charFrequency = new HashMap<>();
+    for (Character oneChar : example.toLowerCase().toCharArray()) {
+      if (!charFrequency.containsKey(oneChar)) {
+        charFrequency.put(oneChar, 1);
+      } else {
+        charFrequency.put(oneChar, charFrequency.get(oneChar) + 1);
+      }
+    }
+    return charFrequency;
   }
 }
