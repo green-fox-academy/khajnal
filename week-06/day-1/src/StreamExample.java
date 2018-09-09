8c25ba721598e261b3645b566e4b5a1b65794117
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-09-09 16:57:48 +0200

    more examples added

--- week-06/day-1/src/StreamExample.java
+++ week-06/day-1/src/StreamExample.java
@@ -1,12 +1,16 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamExample {
 
-  //Stream always has a source, which provides the elements.
-  // Second part of the stream is the operations that need to be perfomed on that stream.
-  //Finally the end condition, the terminal operation.
+  /*Stream always has a source, which provides the elements.
+   Second part of the stream is the operations that need to be performed on that stream.
+   Finally the end condition, the terminal operation.
+   */
+
   public static void main(String[] args) {
     List<Person> people = Arrays.asList(
       new Person("Charles", "Dickens", 60),
@@ -25,5 +29,25 @@ public class StreamExample {
       .count();
 
     System.out.println(count);
+// Second example:
+    List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+    List<String> filtered = strings.stream().filter(string -> !string.isEmpty()).collect(Collectors.toList());
+    System.out.println(filtered);
+
+//  Third example:
+
+    Random random = new Random();
+    random.ints().limit(10).forEach(System.out::println);
+    
+// Fourth example:
+//      the ‘map’ method is used to map each element to its corresponding result.
+//      the following code segment prints unique squares of numbers using map.
+
+    List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+    //get list of unique squares
+    List<Integer> squaresList = numbers.stream().map( i -> i * i).distinct().collect(Collectors.toList());
+    System.out.println();
+    System.out.println("Unique squares: ");
+    System.out.println(squaresList);
   }
 }
