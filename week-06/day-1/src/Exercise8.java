208b67e411b551cde2d8971dcb96ab8fa1af7f74
Krisztina Hajnal <hajnal.krisztina03@gmail.com>
2018-09-10 13:45:59 +0200

    method with stream inside added

--- week-06/day-1/src/Exercise8.java
+++ week-06/day-1/src/Exercise8.java
@@ -2,10 +2,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Exercise8 {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+
+    Map<Integer, Long> nrFrequency = numbers.stream()
+      .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+    System.out.println(nrFrequency);
 
     System.out.println(nrFrequencyMethod(numbers));
   }
