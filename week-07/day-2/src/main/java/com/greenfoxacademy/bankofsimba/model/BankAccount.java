34995a21e7e8fd82c053e881052058629624944b
khajnal <hajnal.krisztina03@gmail.com>
2018-09-17 20:05:13 +0200

    Changed class name

--- week-07/day-2/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
+++ week-07/day-2/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
@@ -1,11 +1,11 @@
 package com.greenfoxacademy.bankofsimba.model;
 
-public class BankApplication {
+public class BankAccount {
   private String name;
   private int balance;
   private String animalType;
 
-  public BankApplication(String name, int balance, String animalType) {
+  public BankAccount(String name, int balance, String animalType) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
