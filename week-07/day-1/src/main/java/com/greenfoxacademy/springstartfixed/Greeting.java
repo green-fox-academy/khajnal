b2e5bda4bf73ef10e2bdaed634cdc0ab22a5484b
khajnal <hajnal.krisztina03@gmail.com>
2018-09-17 15:30:59 +0200

    upgraded greet method works

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstartfixed/Greeting.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstartfixed/Greeting.java
@@ -1,8 +1,8 @@
 package com.greenfoxacademy.springstartfixed;
 
 public class Greeting {
-  long id;
-  String content;
+  private long id;
+  private String content;
 
   public long getId() {
     return id;
--- week-07/day-1/src/main/java/com/greenfoxacademy/springstartfixed/controllers/HelloRESTController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstartfixed/controllers/HelloRESTController.java
@@ -2,14 +2,22 @@ package com.greenfoxacademy.springstartfixed.controllers;
 
 import com.greenfoxacademy.springstartfixed.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
-  @RequestMapping
-  public Greeting greeting() {
-    Greeting greeting = new Greeting(1, "Hello, World!");
+//  @RequestMapping("/greeting")
+//  public Greeting greeting() {
+//    Greeting greeting = new Greeting(1, "Hello, World!");
+//    return greeting;
+//  }
+
+  @RequestMapping("/greeting")
+  public Greeting greeting(@RequestParam(value = "name", required = true) String content){
+    Greeting greeting = new Greeting(1, "Hello, " + content);
     return greeting;
   }
 }
