3dfde99096a7ed9fa190400f192573275593e3c1
khajnal <hajnal.krisztina03@gmail.com>
2018-09-17 17:23:42 +0200

    web greet counter works

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstartfixed/controllers/HelloWebController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstartfixed/controllers/HelloWebController.java
@@ -1,14 +1,26 @@
 package com.greenfoxacademy.springstartfixed.controllers;
 
+import com.greenfoxacademy.springstartfixed.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWebController {
-  @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
-    return "greeting";
+
+//  @RequestMapping("/web/greeting")
+//  public String greeting(Model model) {
+//    model.addAttribute("name", " World");
+//    return "greeting";
+//  }
+
+  @RequestMapping("web/greeting")
+  @ResponseBody
+  public String greeting(@RequestParam(value = "name", required = true) String content) {
+    Greeting greeting = new Greeting(content);
+    return "Hello " + greeting.getContent() + "! This page was loaded " + greeting.getId() + " times.";
   }
 }
