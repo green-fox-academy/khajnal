8a96bdc2f3b4816f7092b2893601ab85f6528f1e
khajnal <hajnal.krisztina03@gmail.com>
2018-09-17 19:18:09 +0200

    BankApplication constructor created

--- week-07/day-2/src/main/java/com/greenfoxacademy/bankofsimba/model/BankApplication.java
+++ week-07/day-2/src/main/java/com/greenfoxacademy/bankofsimba/model/BankApplication.java
@@ -3,5 +3,11 @@ package com.greenfoxacademy.bankofsimba.model;
 public class BankApplication {
   private String name;
   private int balance;
-  private int animalType;
+  private String animalType;
+
+  public BankApplication(String name, int balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
 }
