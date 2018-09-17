1b0c16709a63c0ac64a25427f0ff7c3b58c1eb93
khajnal <hajnal.krisztina03@gmail.com>
2018-09-17 16:55:03 +0200

    greeting.html and HelloWebController init

---
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstartfixed/controllers/HelloWebController.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.springstartfixed.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class HelloWebController {
+  @RequestMapping("/web/greeting")
+  public String greeting(Model model) {
+    model.addAttribute("name", " World");
+    return "greeting";
+  }
+}
