e8143c4908c4644d7c71ea5b758da94f6a04bad0
khajnal <hajnal.krisztina03@gmail.com>
2018-09-18 14:20:54 +0200

    /show works

---
+++ week-07/day-2/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.bankofsimba.controller;
+
+import com.greenfoxacademy.bankofsimba.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BankAccountController {
+  BankAccount bankAccount = new BankAccount("simba", 2000, "lion");
+
+  @GetMapping("/show")
+  public String greeting(Model model) {
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("animalType", bankAccount.getAnimalType());
+    return "bankaccount";
+  }
+}
