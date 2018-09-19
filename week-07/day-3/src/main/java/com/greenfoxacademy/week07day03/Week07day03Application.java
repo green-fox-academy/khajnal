be15091486fbbf365f11dbd0bc75147d557548be
khajnal <hajnal.krisztina03@gmail.com>
2018-09-19 13:13:18 +0200

    run method works

--- week-07/day-3/src/main/java/com/greenfoxacademy/week07day03/Week07day03Application.java
+++ week-07/day-3/src/main/java/com/greenfoxacademy/week07day03/Week07day03Application.java
@@ -1,12 +1,22 @@
 package com.greenfoxacademy.week07day03;
 
+import com.greenfoxacademy.week07day03.service.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Week07day03Application {
+public class Week07day03Application implements CommandLineRunner {
 
+@Autowired
+Printer printer;
   public static void main(String[] args) {
     SpringApplication.run(Week07day03Application.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello");
   }
 }
