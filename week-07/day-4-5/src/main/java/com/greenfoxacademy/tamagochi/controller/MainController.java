54976c00ba1840c6b7bea9397f7eebe0fedc0a45
khajnal <hajnal.krisztina03@gmail.com>
2018-09-20 12:13:09 +0200

    Login form ready, fox class init

--- week-07/day-4-5/src/main/java/com/greenfoxacademy/tamagochi/controller/MainController.java
+++ week-07/day-4-5/src/main/java/com/greenfoxacademy/tamagochi/controller/MainController.java
@@ -1,11 +1,29 @@
 package com.greenfoxacademy.tamagochi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class MainController {
 
   @GetMapping("/")
-  public String getMain() {
+  public String getIndex(@RequestParam(value = "name", required = false) String foxNameInput, Model model) {
+    if (foxNameInput == null)
+      return "login";
+    model.addAttribute("nameOfFox", foxNameInput);
     return "index";
+  }
+
+  @PostMapping("/login")
+  public String loggedIn(@RequestParam(value = "foxname") String foxNameInput) {
+    return "redirect:/?name=" + foxNameInput;
+  }
+
+  @GetMapping("/login")
+  public String login() {
+    return "login";
   }
 }
---
+++ week-07/day-4-5/src/main/java/com/greenfoxacademy/tamagochi/model/Fox.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.tamagochi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fox {
+  String name;
+  List<String> allTricks = new ArrayList<>();
+  String food;
+  String drink;
+}
