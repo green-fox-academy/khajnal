a7ea9248b85189c8e6fd9bc53abace033202c6eb
khajnal <hajnal.krisztina03@gmail.com>
2018-09-25 12:43:11 +0200

    Todo with embedded database init

---
+++ week-08/day-2/src/main/java/com/greenfoxacademy/todoembeddeddata/TodoembeddeddataApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todoembeddeddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoembeddeddataApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodoembeddeddataApplication.class, args);
+  }
+}
