d506fca7da031f0f0135c08d5002141166fcd28e
khajnal <hajnal.krisztina03@gmail.com>
2018-09-27 22:44:57 +0200

    styleguide check, fixed indentations etc.

--- week-08/day-2/todomysql/src/main/java/com/greenfoxacademy/todomysql/model/Assignee.java
+++ week-08/day-2/todomysql/src/main/java/com/greenfoxacademy/todomysql/model/Assignee.java
@@ -1,9 +1,6 @@
 package com.greenfoxacademy.todomysql.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Assignee {
