47da8c9484e4755b16e6ad2458641853e3298cad
khajnal <hajnal.krisztina03@gmail.com>
2018-09-27 20:38:37 +0200

    Refactor(TodoController): unnecessary lines deleted

--- week-08/day-2/todomysql/src/main/java/com/greenfoxacademy/todomysql/services/TodoServiceImpl.java
+++ week-08/day-2/todomysql/src/main/java/com/greenfoxacademy/todomysql/services/TodoServiceImpl.java
@@ -5,7 +5,6 @@ import com.greenfoxacademy.todomysql.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
