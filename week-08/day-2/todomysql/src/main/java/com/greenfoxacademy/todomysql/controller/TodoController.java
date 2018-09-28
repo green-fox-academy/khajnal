28fbf0b0b55f7c605ad7c14876bddd913853b498
khajnal <hajnal.krisztina03@gmail.com>
2018-09-28 13:34:11 +0200

    Comment deleted

--- week-08/day-2/todomysql/src/main/java/com/greenfoxacademy/todomysql/controller/TodoController.java
+++ week-08/day-2/todomysql/src/main/java/com/greenfoxacademy/todomysql/controller/TodoController.java
@@ -20,7 +20,7 @@ public class TodoController {
   }
 
   @GetMapping(value = {"/list", "/"})
-  public String list(@RequestParam(value = "isActive", required = false) Boolean isActive, @RequestParam(value = "value", required = false) String searchedValue, Model model) {
+  public String list(@RequestParam(value = "isActive", required = false) boolean isActive, @RequestParam(value = "value", required = false) String searchedValue, Model model) {
     model.addAttribute("todolist", todoService.getProperTodos(isActive, searchedValue));
     return "todoslist";
   }
