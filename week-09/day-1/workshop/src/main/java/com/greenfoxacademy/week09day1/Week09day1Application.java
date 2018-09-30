b23cff02505de3539496dd455452456a7ba067f6
khajnal <hajnal.krisztina03@gmail.com>
2018-09-30 23:16:34 +0200

    Frontend imported

---
+++ week-09/day-1/workshop/src/main/java/com/greenfoxacademy/week09day1/Week09day1Application.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.week09day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week09day1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Week09day1Application.class, args);
+  }
+}
