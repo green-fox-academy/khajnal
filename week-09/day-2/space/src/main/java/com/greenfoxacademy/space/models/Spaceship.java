bf3af03ecb9d402030e455975f9d7cb62e058a61
khajnal <hajnal.krisztina03@gmail.com>
2018-10-02 16:58:04 +0200

    Spaceship model class created with field variables, getters-setters

---
+++ week-09/day-2/space/src/main/java/com/greenfoxacademy/space/models/Spaceship.java
@@ -0,0 +1,58 @@
+package com.greenfoxacademy.space.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Spaceship {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+  int maxCapacity;
+  String planet;
+  int utilization;
+
+  public Spaceship() {
+  }
+
+  public Spaceship(int maxCapacity, String planet, int utilization) {
+    this.maxCapacity = maxCapacity;
+    this.planet = planet;
+    this.utilization = utilization;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public int getMaxCapacity() {
+    return maxCapacity;
+  }
+
+  public void setMaxCapacity(int maxCapacity) {
+    this.maxCapacity = maxCapacity;
+  }
+
+  public String getPlanet() {
+    return planet;
+  }
+
+  public void setPlanet(String planet) {
+    this.planet = planet;
+  }
+
+  public int getUtilization() {
+    return utilization;
+  }
+
+  public void setUtilization(int utilization) {
+    this.utilization = utilization;
+  }
+}
